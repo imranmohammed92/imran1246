7e335d49917b2ea6b30a53a3919a86ec2a4f2a1a
imranmohammed92 <imranmohammed1246@gmail.com>
2023-12-28 13:54:13 -0600

    HotelManagementApp day01 task

---
+++ src/com/practice/HotelManagement/HotelManagementApp.java
@@ -0,0 +1,31 @@
+package com.practice.HotelManagement;
+
+public class HotelManagementApp {
+    public static void main(String[] args) {
+        /*
+        Day 01:
+
+hotel booking management
+
+ Please select your preffered choice
+
+	1. register account
+	2. book reservation
+	3. generate bill
+	4. feedback
+         */
+        System.out.println("***********************************");
+        System.out.println("Welcome to Hotel Booking Management");
+        System.out.println("***********************************");
+
+
+        System.out.println("Please Select your Preferred Choice:");
+        System.out.println("1. Register Account");
+        System.out.println("2. Book Reservation");
+        System.out.println("3. Generate Bill");
+        System.out.println("4.Feedback");
+
+
+
+    }
+}
